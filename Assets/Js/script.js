de97c0f896903edb9c31803662d667765265b892
Jane Ngugi <57115546+janyta675@users.noreply.github.com>
2024-07-08 13:26:16 +0300

    update index.html and css

---
+++ Assets/Js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const balanceElement = document.querySelector('.balance');
+    const numberButtons = document.querySelectorAll('.number-btn');
+    const amountInput = document.getElementById('amount');
+    const resultElement = document.querySelector('.result');
+    const inputError = document.querySelector('.inputerror');
+    const numberSelectionError = document.querySelector('.numberselectionerror');
+    const placeBetButton = document.getElementById('placeBet');
+    const diceImage = document.getElementById('diceimage');
+
+    
+}
+);
